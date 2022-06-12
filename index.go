520f45c0760da01070c9d5281dcb65793ef1e102
chauhan17nitin <nitinchauhan1799@gmail.com>
2022-06-12 18:28:26 +0530

    fix: testing changes complete

--- index.go
+++ index.go
@@ -18,19 +18,7 @@ type Test2 struct {
 	Test  string `json:"Test"`
 }
 
-type Embed struct {
-	Name  string `json:"Name"`
-	Value string `json:"Value"`
-}
-
-type Foo struct {
-	Number int
-	Text   string
-	fumber int
-}
-
 func main() {
-	// foo := Foo{123, "Hello", 111}
 	test := Test{
 		Name:  "Nitin",
 		Value: 5,
@@ -50,16 +38,6 @@ func main() {
 	return
 }
 
-func overrideStruct(v interface{}, t interface{}) interface{} {
-	old := reflect.ValueOf(t)
-	fmt.Println(old.FieldByName("fumber"))
-	abc := reflect.New(reflect.Indirect(reflect.ValueOf(v)).Type()).Elem()
-	fmt.Println(abc.Kind())
-	test := old.FieldByName("fumber").Int()
-	abc.FieldByName("Number").SetInt(test)
-	return abc.Interface()
-}
-
 func overrideStructFinal(input interface{}, output interface{}) interface{} {
 	// if both of them are not of struct type then you need to panic
 	inputValue := reflect.ValueOf(input)
@@ -70,48 +48,12 @@ func overrideStructFinal(input interface{}, output interface{}) interface{} {
 		panic("Input and output both must be of struct types")
 	}
 
-	// test := dummyOutput.Field(1)
-	// test.Set(4)
-
 	for i := 0; i < dummyOutput.NumField(); i++ {
 		field := dummyOutput.Field(i)
-		fmt.Println(outputType.Field(i))
 		value := inputValue.FieldByName(outputType.Field(i).Name)
 
 		field.Set(value)
-		// fmt.Println()
 	}
 
 	return dummyOutput.Interface()
-}
-
-func parseStruct(input interface{}, output interface{}) {
-	typeInput := reflect.ValueOf(input)
-	typeOutput := reflect.ValueOf(output).Type()
-	fmt.Println(typeInput.Kind())
-	fmt.Println(typeOutput.Kind())
-	// fmt.Println(typeInput.)
-	value := reflect.ValueOf(input)
-
-	if value.Kind() == reflect.Struct {
-		f := value.FieldByName("Name")
-		fmt.Println("printing the fields")
-		if f.IsValid() {
-			fmt.Println("fucking yes")
-			if f.CanSet() {
-				fmt.Println("yes can be changed")
-			}
-		} else {
-			fmt.Println("damn no")
-		}
-	}
-	fmt.Println(value.Kind())
-
-	// fmt.Println(reflect.ValueOf(input).FieldByName("Name").CanSet())
-	// // fmt.Println(reflect.ValueOf(output).Set(value))
-	// fmt.Println(reflect.ValueOf(output).FieldByName("Name").CanSet())
-	// fmt.Println(typeInput.Field(0))
-	// fmt.Println(typeInput.Field(1))
-	// fmt.Println(typeInput.Field(3))
-
 }
