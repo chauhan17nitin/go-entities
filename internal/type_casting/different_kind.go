aaeb8056ca4721b5da764618e4cced5283bc8093
chauhan17nitin <nitinchauhan1799@gmail.com>
2022-06-14 01:26:48 +0530

    fia: add int, float, uint cross type conversion

--- internal/type_casting/different_kind.go
+++ internal/type_casting/different_kind.go
@@ -1,6 +1,8 @@
 package typecasting
 
-import "reflect"
+import (
+	"reflect"
+)
 
 var allowedInts = map[reflect.Kind]struct{}{
 	reflect.Int:   {},
@@ -10,15 +12,17 @@ var allowedInts = map[reflect.Kind]struct{}{
 	reflect.Int64: {},
 }
 
-var allowedIntCasts = map[reflect.Kind]struct{}{
-	reflect.Int:   {},
-	reflect.Int8:  {},
-	reflect.Int16: {},
-	reflect.Int32: {},
-	reflect.Int64: {},
-
+var allowedFloats = map[reflect.Kind]struct{}{
 	reflect.Float32: {},
 	reflect.Float64: {},
+}
+
+var allowedUints = map[reflect.Kind]struct{}{
+	reflect.Uint:   {},
+	reflect.Uint8:  {},
+	reflect.Uint16: {},
+	reflect.Uint32: {},
+	reflect.Uint64: {},
 }
 
 func CastDifferentKind(field, value *reflect.Value) {
@@ -38,11 +42,33 @@ func intCasting(field, value *reflect.Value) {
 		panic("Can not cast non int type to int")
 	}
 
-	if _, ok := allowedIntCasts[value.Type().Kind()]; !ok {
+	if _, ok := allowedInts[value.Type().Kind()]; !ok {
 		panic("Can not cast non int type to int")
 	}
 
-	if field.Type().Kind() == value.Type().Kind() {
-		field.Set(*value)
+	field.SetInt(value.Int())
+}
+
+func uintCasting(field, value *reflect.Value) {
+	if _, ok := allowedUints[field.Type().Kind()]; !ok {
+		panic("Can not cast non float type to float")
 	}
+
+	if _, ok := allowedUints[field.Type().Kind()]; !ok {
+		panic("Can not cast non float type to float")
+	}
+
+	field.SetUint(value.Uint())
+}
+
+func floatCasting(field, value *reflect.Value) {
+	if _, ok := allowedFloats[field.Type().Kind()]; !ok {
+		panic("Can not cast non float type to float")
+	}
+
+	if _, ok := allowedFloats[field.Type().Kind()]; !ok {
+		panic("Can not cast non float type to float")
+	}
+
+	field.SetFloat(value.Float())
 }
--- internal/type_casting/different_kind_test.go
+++ internal/type_casting/different_kind_test.go
@@ -7,11 +7,11 @@ import (
 
 func Test_intCasting(t *testing.T) {
 	type input struct {
-		Test int
+		Test int32
 	}
 
 	type output struct {
-		Test int
+		Test int64
 	}
 
 	testInput := input{
@@ -27,4 +27,40 @@ func Test_intCasting(t *testing.T) {
 	value := inputReflect.FieldByName("Test")
 
 	intCasting(&field, &value)
+
+	finalOutput := dummyOutput.Interface()
+
+	if finalOutput.(output).Test != 10 {
+		t.Errorf("Failed Int casting without panic")
+	}
+}
+
+func Test_floatCasting(t *testing.T) {
+	type input struct {
+		Test float32
+	}
+
+	type output struct {
+		Test float64
+	}
+
+	testInput := input{
+		Test: 29.5,
+	}
+
+	testOutput := output{}
+
+	inputReflect := reflect.ValueOf(testInput)
+	dummyOutput := reflect.New(reflect.Indirect(reflect.ValueOf(testOutput)).Type()).Elem()
+
+	field := dummyOutput.FieldByName("Test")
+	value := inputReflect.FieldByName("Test")
+
+	floatCasting(&field, &value)
+
+	finalOutput := dummyOutput.Interface()
+
+	if finalOutput.(output).Test != 29.5 {
+		t.Errorf("Failed Int casting without panic")
+	}
 }
