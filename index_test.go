3f340e6c6d10dd60742cf774e19218eecb6296c7
Nitin Chauhan <39927335+chauhan17nitin@users.noreply.github.com>
2022-06-18 20:09:12 +0530

    Feat/improve flow and test cases (#9)
    * feat: adding test cases
    
    * feat: adding tests workflow

---
+++ index_test.go
@@ -0,0 +1,90 @@
+package goentities
+
+import (
+	"testing"
+)
+
+func Test_PresentSimple(t *testing.T) {
+	type input struct {
+		IntField    int
+		FloatField  float32
+		StringField string
+		UintField   uint
+	}
+
+	type output struct {
+		IntField    int32
+		FloatField  float64
+		StringField string
+		UintField   uint64
+	}
+
+	testInput := input{
+		IntField:    -5,
+		FloatField:  5.5,
+		StringField: "this is a test string",
+		UintField:   5,
+	}
+
+	testOutput := output{}
+
+	outputValue := Present(testInput, testOutput)
+	castedOutput := outputValue.(output)
+
+	if castedOutput.IntField != int32(testInput.IntField) {
+		t.Errorf("Failed in casting int field")
+	}
+
+	if castedOutput.FloatField != float64(testInput.FloatField) {
+		t.Errorf("Failed in Float casting")
+	}
+
+	if castedOutput.StringField != testInput.StringField {
+		t.Errorf("Failed in String casting")
+	}
+
+	if castedOutput.UintField != uint64(testInput.UintField) {
+		t.Errorf("Failed in UInt casting")
+	}
+}
+
+func Test_PresentStructNesting(t *testing.T) {
+	type inputNesting struct {
+		IntField    int
+		StringField string
+	}
+
+	type input struct {
+		NestingField inputNesting
+	}
+
+	type outputNesting struct {
+		IntField    int64
+		StringField string
+	}
+
+	type output struct {
+		NestingField outputNesting
+	}
+
+	testInput := input{
+		inputNesting{
+			IntField:    10,
+			StringField: "test string",
+		},
+	}
+
+	testOutput := output{}
+
+	outputValue := Present(testInput, testOutput)
+	castedOutput := outputValue.(output)
+
+	if castedOutput.NestingField.IntField != int64(testInput.NestingField.IntField) {
+		t.Errorf("Failed Casting Nested Struct Int Field")
+	}
+
+	if castedOutput.NestingField.StringField != testInput.NestingField.StringField {
+		t.Errorf("Failed Casting Nested Struct String Field")
+	}
+
+}
